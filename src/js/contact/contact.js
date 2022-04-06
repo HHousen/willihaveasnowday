9844ad4817770b8f9729b4a38276a6c33f3d7a4d
Hayden Housen <hayden@haydenhousen.com>
2022-04-06 16:34:38 -0400

    Disable contact form

--- src/js/contact/contact.js
+++ src/js/contact/contact.js
@@ -1,27 +1,6 @@
-var form = document.querySelector('.pageclip-form')
-Pageclip.form(form, {
-    onResponse: function (error, response) {
-        if (response != null && response.data == "ok") {
-            M.toast({
-                html: "Your message has been sent."
-            });
-            setTimeout(function() {
-                M.toast({
-                    html: "Redirecting home..."
-                });
-            }, 1700);
-            setTimeout(function() {
-                window.location.replace("/");
-            }, 2000);
-        } else if (error != null) {
-            M.toast({
-                html: "Your message failed to send. " + error.toString()
-            });
-        } else {
-            M.toast({
-                html: "Your message failed to send. An unknown error occurred."
-            });
-        }
-        return false;
-    }
-})
+$(".pageclip-form").submit(function (e) {
+    e.preventDefault();
+    M.toast({
+        html: "This contact form has been disabled."
+    });
+});
